cf4b8ea6ae17bf45558965760a99492880b9a5f0
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2023-03-15 12:17:51 +0200

    create food order app, added all files

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighligted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfItems = items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighligted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighligted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onShowCart}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
